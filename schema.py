06191c3198cf586d87114593cfeacd7efb5909a7
BloodRight13 <166233088+BloodRight13@users.noreply.github.com>
2025-01-28 17:47:45 -0600

    Update schema.py

--- schema.py
+++ schema.py
@@ -1,7 +1,8 @@
 import graphene
+from model import get_products, get_product_by_id, add_product, update_product, delete_product
 
 class Product(graphene.ObjectType):
-    id = graphene.ID()
+    id = graphene.Int()
     name = graphene.String()
     price = graphene.Float()
     quantity = graphene.Int()
@@ -9,18 +10,15 @@ class Product(graphene.ObjectType):
 
 class Query(graphene.ObjectType):
     products = graphene.List(Product)
-    product = graphene.Field(Product, id=graphene.ID(required=True))
+    product = graphene.Field(Product, id=graphene.Int(required=True))
 
-    def resolve_products(root, info):
-        return products_data
+    def resolve_products(self, info):
+        return get_products()
 
-    def resolve_product(root, info, id):
-        for product in products_data:
-            if product['id'] == id:
-                return product
-        return None
+    def resolve_product(self, info, id):
+        return get_product_by_id(id)
 
-class CreateProduct(graphene.Mutation):
+class AddProduct(graphene.Mutation):
     class Arguments:
         name = graphene.String(required=True)
         price = graphene.Float(required=True)
@@ -29,15 +27,14 @@ class CreateProduct(graphene.Mutation):
 
     product = graphene.Field(Product)
 
-    def mutate(root, info, name, price, quantity, category):
-        new_id = str(len(products_data) + 1)
-        product = {'id': new_id, 'name': name, 'price': price, 'quantity': quantity, 'category': category}
-        products_data.append(product)
-        return CreateProduct(product=product)
+    def mutate(self, info, name, price, quantity, category):
+      product = add_product(name, price, quantity, category)
+      return AddProduct(product=product)
+
 
 class UpdateProduct(graphene.Mutation):
     class Arguments:
-        id = graphene.ID(required=True)
+        id = graphene.Int(required=True)
         name = graphene.String()
         price = graphene.Float()
         quantity = graphene.Int()
@@ -45,42 +42,24 @@ class UpdateProduct(graphene.Mutation):
 
     product = graphene.Field(Product)
 
-    def mutate(root, info, id, name=None, price=None, quantity=None, category=None):
-        for product in products_data:
-            if product['id'] == id:
-                if name:
-                    product['name'] = name
-                if price:
-                    product['price'] = price
-                if quantity:
-                    product['quantity'] = quantity
-                if category:
-                    product['category'] = category
-                return UpdateProduct(product=product)
-        return None
+    def mutate(self, info, id, name=None, price=None, quantity=None, category=None):
+      product = update_product(id, name, price, quantity, category)
+      return UpdateProduct(product=product)
+
 
 class DeleteProduct(graphene.Mutation):
     class Arguments:
-        id = graphene.ID(required=True)
+        id = graphene.Int(required=True)
 
     ok = graphene.Boolean()
 
-    def mutate(root, info, id):
-        global products_data
-        products_data = [product for product in products_data if product['id'] != id]
-        return DeleteProduct(ok=True)
-
+    def mutate(self, info, id):
+        result = delete_product(id)
+        return DeleteProduct(ok=result)
 
 class Mutation(graphene.ObjectType):
-    create_product = CreateProduct.Field()
+    add_product = AddProduct.Field()
     update_product = UpdateProduct.Field()
     delete_product = DeleteProduct.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
-
-# Dummy data for the bakery inventory
-products_data = [
-    {'id': '1', 'name': 'Chocolate Cake', 'price': 25.0, 'quantity': 10, 'category': 'Cakes'},
-    {'id': '2', 'name': 'Croissant', 'price': 3.5, 'quantity': 50, 'category': 'Pastries'},
-    {'id': '3', 'name': 'Baguette', 'price': 4.0, 'quantity': 30, 'category': 'Bread'},
-]
